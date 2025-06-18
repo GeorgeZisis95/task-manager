cc6b69873a69f703b1022e1781ce09964ac5df3d
George Zisis <georgezisis@gmail.com>
2025-06-18 11:14:26 +0300

    Add function to show-hide sidebar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css"
+
+const sidebarFullButton = document.querySelector(".sidebar-full .sidebar-icon")
+const sidebarSmallButton = document.querySelector(".sidebar-small .sidebar-icon")
+
+const sidebarFull = document.querySelector(".sidebar-full")
+const sidebarSmall = document.querySelector(".sidebar-small")
+
+sidebarFullButton.addEventListener("click", () => {
+    sidebarFull.classList.add("hide")
+    sidebarSmall.classList.remove("hide")
+})
+
+sidebarSmallButton.addEventListener("click", () => {
+    sidebarFull.classList.remove("hide")
+    sidebarSmall.classList.add("hide")
+})
