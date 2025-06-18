89344f860cde6f078590f8e1187d3610b94aed6a
George Zisis <georgezisis@gmail.com>
2025-06-18 12:11:54 +0300

    Create responsive sidebar

--- src/index.js
+++ src/index.js
@@ -7,11 +7,34 @@ const sidebarFull = document.querySelector(".sidebar-full")
 const sidebarSmall = document.querySelector(".sidebar-small")
 
 sidebarFullButton.addEventListener("click", () => {
-    sidebarFull.classList.add("hide")
-    sidebarSmall.classList.remove("hide")
+    toSidebarSmall()
 })
 
 sidebarSmallButton.addEventListener("click", () => {
-    sidebarFull.classList.remove("hide")
-    sidebarSmall.classList.add("hide")
+    toSidebarFull()
 })
+
+const mediaQuery = window.matchMedia("(max-width:700px)")
+if (mediaQuery.matches) {
+    sidebarSmall.classList.remove("hide")
+    sidebarFull.classList.add("hide")
+}
+mediaQuery.addEventListener("change", () => {
+    if (mediaQuery.matches) {
+        toSidebarSmall()
+    } else {
+        toSidebarFull()
+    }
+})
+
+function toSidebarSmall() {
+    sidebarFull.classList.add("move-left")
+    sidebarSmall.classList.remove("hide")
+    sidebarSmall.classList.remove("move-right")
+}
+
+function toSidebarFull() {
+    sidebarFull.classList.remove("move-left")
+    sidebarFull.classList.remove("hide")
+    sidebarSmall.classList.add("move-right")
+}
